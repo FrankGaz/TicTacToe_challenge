bdef0c30c47bb7cac611c22a79702c59b0c90f2f
Frank Artacho <frankar88@gmail.com>
2022-03-25 14:08:36 +0100

    Commit 4 - Adds logic to end the game in App.js

--- tictactoe-challenge/src/App.js
+++ tictactoe-challenge/src/App.js
@@ -2,6 +2,18 @@ import { useState } from "react";
 import "./styles/main.scss";
 import Board from "./components/Board";
 
+// Possible winning positions
+const winningPositions = [
+  [0, 1, 2],
+  [3, 4, 5],
+  [6, 7, 8],
+  [0, 3, 6],
+  [1, 4, 7],
+  [2, 5, 8],
+  [0, 4, 8],
+  [2, 4, 6],
+];
+
 const App = () => {
   // State for the player turn
   const [turn, setTurn] = useState("X");
@@ -15,8 +27,43 @@ const App = () => {
     O: 0,
   });
 
-  const checkForWinner = (squares) => {
+  // State for winnin set of squares
+  const [winningSquares, setWinningSquares] = useState([]);
+
+  // Checking for winning positions
+  const checkForWinner = (newSquares) => {
+    for (let i = 0; i < winningPositions.length; i++) {
+      const [a, b, c] = winningPositions[i];
+      if (
+        newSquares[a] &&
+        newSquares[a] === newSquares[b] &&
+        newSquares[a] === newSquares[c]
+      ) {
+        console.log("There is a winner");
+        endGame(newSquares[a], winningPositions[i]);
+        console.log("Winner >>> ", newSquares[a]);
+        return;
+      }
+    }
+    // Check if there is a tie
+    if (!newSquares.includes(null)) {
+      console.log("There is a tie");
+      endGame(null, Array.from(Array(10).keys()));
+      return;
+    }
     setTurn(turn === "X" ? "O" : "X");
+  };
+
+  // End Game function
+  const endGame = (result, winningPositions) => {
+    setTurn(null);
+    if (result !== null) {
+      setScore({
+        ...score,
+        [result]: score[result] + 1,
+      });
+    }
+    setWinningSquares(winningPositions);
   };
 
   const handleClick = (square) => {
@@ -28,7 +75,12 @@ const App = () => {
 
   return (
     <div className="container">
-      <Board turn={turn} squares={squares} onClick={handleClick} />
+      <Board
+        winningSquares={winningSquares}
+        turn={turn}
+        squares={squares}
+        onClick={handleClick}
+      />
     </div>
   );
 };
